e2d5281fc3f99600731068bf2d8079757cf164fb
1988fuyu@163.com <1988fuyu@163.com>
2016-03-15 23:53:05 +0800

    题目

--- act_NYFestival/js/ act_16QMing_gamecon.js
+++ act_NYFestival/js/ act_16QMing_gamecon.js
@@ -1,36 +1,88 @@
 /**
  * Created by gonglei.bian on 2016/2/19.
  */
+
+var QUESTIONS = [
+			{"id":10000 ,"question":"清明时节雨纷纷,","options":[{"id":10001,"option":"A、路上行人像断魂"},{"id":10002,"option":"B、路上行人欲断魂"},{"id":10003,"option":"C、路上行人在招魂"},{"id":10004,"option":"D、路上行人丢了魂"}], "answer":10002},
+			{"id":20000 ,"question":"借问酒家何处有,","options":[{"id":20001,"option":"A、牧童遥指稻香村"},{"id":20002,"option":"B、酒香不怕巷子深"},{"id":20003,"option":"C、牧童遥指酒鬼村"},{"id":20004,"option":"D、牧童遥指杏花村"}], "answer":20004},
+			{"id":30000 ,"question":",兴味萧然似野僧","options":[{"id":30001,"option":"A、无花无酒过清明"},{"id":30002,"option":"B、一壶好酒过清明"},{"id":30003,"option":"C、孤帆远影碧空尽"},{"id":30004,"option":"D、喝点小酒来点肉"}], "answer":30001},
+			{"id":40000 ,"question":",田野荒冢只生愁","options":[{"id":40001,"option":"A、一骑红尘妃子笑"},{"id":40002,"option":"B、佳节清明桃李笑"},{"id":40003,"option":"C、佳节清明旧人笑"},{"id":40004,"option":"D、屋里对镜咧嘴笑"}], "answer":40002},
+			{"id":50000 ,"question":"南北山头多墓田,","options":[{"id":50001,"option":"A、西北坟头多草甸"},{"id":50002,"option":"B、清明祭扫多泪流"},{"id":50003,"option":"C、清明祭扫各纷然"},{"id":50004,"option":"D、东西山头多坟墓"}], "answer":50003},
+			{"id":60000 ,"question":"素衣莫起风尘叹,","options":[{"id":60001,"option":"A、犹及清明可到家"},{"id":60002,"option":"B、锦衣清明就回家"},{"id":60003,"option":"C、清明还不能回家"},{"id":60004,"option":"D、清明祭扫小长假"}], "answer":60001},
+			{"id":70000 ,"question":"帝里重清明,",    "options":[{"id":70001,"option":"A、郊外重哀思"},    {"id":70002,"option":"B、不再忘忧思"},   {"id":70003,"option":"C、人心自愁思"},    {"id":70004,"option":"D、节过更愁思"}],     "answer":70003},
+			{"id":80000 ,"question":"好风胧月清明夜,","options":[{"id":80001,"option":"A、大红灯笼高高挂"},{"id":80002,"option":"B、灯火通明官府家"},{"id":80003,"option":"C、关好门窗少进风"},{"id":80004,"option":"D、碧砌红轩刺史家"}], "answer":80004}
+	]
+
+var SUCCESS = 0 ;
+
 $(function(){
+	initQuestion() ;
+	function initQuestion(index){
+		var QNo = '<span>题目：</span>' ;
+		var QSpase = '<p class="ques_userCop">&nbsp;</p>' ;
+		var QTitle = '<p class="ques_remind" >{title}</p>' ;
 	
+		var QOption = '<div class="question_queNum" data-answer="{answer}"  data-index="{index}"><span class="radioBtn"></span><p class="quest_verse">{opt}</p></div>' ;
 	
-	$.getJSON("./js/act_16QMing_questions.json", function(data){
-		  var questionsData = data.questions ;
-		  $.each(questionsData , function(item_index ,item){
-		  	$question = item ;
-	  		var ops = '' ;
-	  		$options = $question.options ;
-	  		$.each($options,function(opt_index,option){
-	  			$option = option ;
-	  			//ops += $option.option + ' ,';
-	  		}) ;
+		var cindex = index || 0 ;
+		
+		var $question = QUESTIONS[cindex] ;
+  		/**正确答案编号*/
+  		var ans = $question.answer ;
+  		/**选项*/
+  		$options = $question.options ;
+  		/**问题*/
+  		$question_title = $question.question ;
+  		
+  		var opts = '' ;
+  		$.each($options,function(opt_index,option){
+  			$option = option ;
+  			/**标识该选项是否正确*/
+  			if(ans == $option.id){
+  				opts += QOption.replace('{answer}' ,true).replace('{opt}' , $option.option).replace('{index}' ,cindex) ;
+  			}else{
+  				opts += QOption.replace('{answer}' ,false).replace('{opt}' , $option.option).replace('{index}',cindex) ;
+  			}
+  		}) ;
 //	  		alert('第'+(item_index+1)+'题 问题是 ，' + $question.question + '\n选项是： '+ ops + "\n答案是： " + $question.answer) ;
-		  	$(".question_numb").html('第'+(item_index+1)+'题 ');
-		  	$(".ques_remind").html($question.question);
-		  	$(".ques_userCop").html($question.question);
-		  	$(".quest_verse").html($option);//
-		  	//$(".quest_verse").html($question.answer);//此处是答案
+		 
+		/**判断标识位置 是在前面还是在后面*/
+	  	if($question_title.indexOf(',') > 0){
+	  		QTitle = QTitle.replace('{title}' , $question_title) ;
+	  		var content = QNo + QTitle + QSpase ; 
+	  	}else{
+	  		QTitle = QTitle.replace('{title}' , $question_title) ;
+	  		var content = QNo + QSpase + QTitle ;
+	  	}
+	  	$(".question_numb").html('第'+(cindex+1)+'题 ');
+	  	$(".question_title").html(content);
+//	  	$(".quest_verse").html(opts);//
+	  	$('#puestion_body').html(opts) ;
+	 // 	$(".ques_userCop").html($question.question);
+	  	//$(".quest_verse").html($question.answer);//此处是答案
 		  	
-		  	
-		  }) ;
-		});
+		$('.question_queNum').unbind('tap') ;
 	
-	
+		$(".question_queNum").on("tap",function(){
+			var nextIndex = parseInt($(this).attr('data-index')) + 1 ;
+			
+			if(nextIndex >= 8){
+				alert("已经是最后一题了哦，您答对了 , " + SUCCESS) ;
+				return  ;
+			}
+			
+			
+			if($(this).attr('data-answer') == 'true'){
+				++SUCCESS ;
+			}
+	        alert(SUCCESS);
+	        /**显示下一题*/
+	        initQuestion(nextIndex) ;
+	    });
+	}
 	
     /*开始*/
-    $(".question_queNum").on("tap",function(){
-        
-    });
+    
 
   
   
