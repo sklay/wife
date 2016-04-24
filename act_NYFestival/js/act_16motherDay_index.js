3befd9cad943166fd321048249d43e5f3c4cfb5d
1988fuyu@163.com <1988fuyu@163.com>
2016-04-24 18:16:36 +0800

    新活动

---
+++ act_NYFestival/js/act_16motherDay_index.js
@@ -0,0 +1,28 @@
+/*页面活动 的显示和影藏*/
+var activity = {};
+
+activity.showPage = function(curpage, nextpage) {
+	$(curpage).hide();
+	$(nextpage).show();
+};
+$(function() {
+	activity.showPage('.controllerPage', '.con_wrap_index');
+
+//	activity.showPage(".con_wrap_index", ".game_show");
+//	$('.common_wrap').hide() ;
+//	$('.game_show_moduel_seven').show() ;
+
+	$('.con_wrap_index .slide_up').on('click', function() {
+		activity.showPage(".con_wrap_index", ".game_show");
+		$('.game_show_moduel_one').show() ;
+	});
+
+	$('.game_show .slide_up').on('click', function() {
+		var curPage = $(this).attr('data-cur');
+		var nextPage = $(this).attr('data-next');
+		activity.showPage(curPage, nextPage);
+	});
+	
+
+});
+
