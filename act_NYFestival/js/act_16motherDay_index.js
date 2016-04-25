418b7b9cfdbdc0220274a8f653a30bdadb8b2c1b
1988fuyu@163.com <1988fuyu@163.com>
2016-04-25 23:23:54 +0800

    整合

--- act_NYFestival/js/act_16motherDay_index.js
+++ act_NYFestival/js/act_16motherDay_index.js
@@ -1,13 +1,13 @@
 /*页面活动 的显示和影藏*/
 var activity = {};
 
-activity.showPage = function(nextpage) {
-	var outClass = 'pt-page-moveToTop';
-	var inClass = 'pt-page-moveFromBottom';
-	$nowPage = $('.page-current');
+activity.showPage = function($nowPage, $nextPage, css) {
 
-	$nextPage = $(nextpage);
-
+	if (!$nextPage.length) {
+		return;
+	}
+	var outClass = css.outClass;
+	var inClass = css.inClass;
 	$nowPage.addClass(outClass);
 	$nextPage.addClass(inClass);
 
@@ -24,7 +24,6 @@ activity.showPage = function(nextpage) {
 		$nextPage.find("img").removeClass("hide").addClass("show");
 		$nextPage.find("div").removeClass("hide").addClass("show");
 	}, 600);
-
 
 };
 
@@ -55,62 +54,53 @@ activity.musicCtrl = function() {
 		}
 	});
 	/**自动播放*/
-	//	$(".music_close").trigger("tap") ;
+	$(".music_close").trigger("tap");
 };
+activity.swipe = function() {
+	$(document).on('swipeDown', function() {
+		$nowPage = $('.page-current');
+		$nextPage = $nowPage.prev('.common_wrap');
+		console.debug("swipeDown ", $nextPage.length);
+		var css = {
+			outClass: 'pt-page-moveToBottom',
+			inClass: 'pt-page-moveFromTop'
+		};
+		activity.showPage($nowPage, $nextPage, css);
+		$('.slide_up').show();
+	});
 
+
+	$(document).on('swipeUp', function() {
+		$nowPage = $('.page-current');
+		$nextPage = $nowPage.next('.common_wrap');
+		console.debug("swipeUp ", $nextPage.length);
+		var css = {
+			inClass: 'pt-page-moveFromBottom',
+			outClass: 'pt-page-moveToTop'
+		};
+		activity.showPage($nowPage, $nextPage, css);
+		/**判断是不是最后一页 是最后一页隐藏箭头*/
+		var $nextAll = $nextPage.nextAll();
+
+		if ($nextAll.length <= 1) {
+			$('.slide_up').hide();
+		} else {
+			$('.slide_up').show();
+		}
+
+		console.debug(" nextAll ", $nextAll.length);
+	})
+}
 $(function() {
 
-
-	//	activity.showPage(".con_wrap_index", ".game_show");
-	//	$('.common_wrap').hide() ;
-	//	$('.game_show_moduel_seven').show() ;
-
-	/*$(document).on('swipeUp', function() {
-		$nowPage = $('.page-current');
-		console.debug("$nowPage length " , $nowPage.length) ;
-		var nextPage = ".game_show_moduel_one" ;
-		if($nowPage.length > 0){
-			nextPage = $nowPage.attr('data-next');;
-		}
-		
-		activity.showPage(nextPage);
-		console.debug("swipeUp  nextPage page is " ,nextPage) ;
-		if('.game_show_moduel_one' == nextPage){
-			console.debug("显示游戏页面的第一页的时候 game_show 地显示 ") ;
-			$('.game_show').show() ;
-		}
-	});
-*/
-	/*	$('.game_show .slide_up').on('click', function() {
-			var curPage = $(this).attr('data-cur');
-			var nextPage = $(this).attr('data-next');
-			activity.showPage(curPage, nextPage);
-		});*/
-
-
-	/*$(document).on('swipeDown', function() {
-			$nowPage = $('.page-current');
-			var beforePage = $nowPage.attr('data-before');
-			console.debug("swipeDown  " ,beforePage) ;
-			if (beforePage){
-				activity.showPage(beforePage);
-				// 下划如果包含 con_wrap_index 则是首页跳转到 game 否则是在game也内切换
-				if($nowPage.hasClass('game_show_moduel_one')){
-					$('.con_wrap_index').show() ;
-					$('.game_show').hide() ;
-				};
-			}
-		})*/
-		/*$('.common_wrap').on("swipeDown", function(e) {
-			var curPage = $(this).attr('data-cur');
-			var nextPage = $(this).attr('data-next');
-			if (curPage && nextPage) {
-				activity.showPage(curPage, nextPage);
-			}
-		})*/
+	/**取消默认滑动事件*/
+	document.addEventListener('touchmove', function(event) {
+		event.preventDefault();
+	}, false);
 
 	/**音乐控制*/
-//	activity.musicCtrl();
+	activity.musicCtrl();
 
-
+	/**上下滑动*/
+	activity.swipe() ;
 });
