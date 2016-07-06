2dad2fcc4df336d623708fd035bf8a6210e880c7
1988fuyu@163.com <1988fuyu@163.com>
2016-07-06 18:22:50 +0800

    球迷

--- act_NYFestival/js/act_16iceBucket_index.js
+++ act_NYFestival/js/act_16iceBucket_index.js
@@ -6,13 +6,13 @@ $(function() {
 	});
 
 	var gameScore = 0; //分数统计
-	var windowAniDownTimeBoo = true; //是否点击
+	var gameContinue = true; //是否点击
 	var num = 0; //指定当前窗口下标
-	var childChoNum = 0; //指定当前出现分数
+//	var childChoNum = 0; //指定当前出现分数
 	var windowAniDownTime = null;
-	var clickTime = null;
 	var musicConSrc = document.getElementById("scorePlus");
-	var scoreOnceBoo = true;
+	console.debug("musicConSrc",musicConSrc);
+//	var scoreOnceBoo = true;
 	var sound_add = document.getElementById("sound_add");
 	//  var sound_cut=document.getElementById("sound_cut");
 	sound_add.volume = 0.5;
@@ -20,28 +20,61 @@ $(function() {
 
 	/*游戏主函数*/
 	function gameStart() {
-		setTimeout(function() {
-			windowAniUp();
-			//          windowAniUpFun();
-		}, 500)
+		
+		gameScore = 0; //分数统计
+		gameContinue = true; //是否点击
+	 	num = 0; //指定当前窗口下标
+//	 	childChoNum = 0; //指定当前出现分数
+	 	windowAniDownTime = null;
+//		scoreOnceBoo = true;
+		
 		timeCountDownFun();
+		
+	//	setTimeout(function() {
+			windowAniUpFun();
+			console.debug("game start") ;
+	//	}, 500)
+		
 	};
+	
+	//窗口随机函数
+    function countWinNumFun(){
+   	 var numRan=Math.floor(Math.random()*12000);
+   	 var numScope=[1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000,12000]
+   	 var numSort=[0,4,3,5,1,2,5,2,4,3,1,0]
+   	 for(var i=0,j=numScope.length;i<j;i++){
+    	 if(numRan<=numScope[i]){
+    		 num=numSort[i];
+    		 break;
+    	 }
+     }  
+   };
+	
 	/*人物弹起*/
 	function windowAniUpFun() {
-		num = Math.floor(Math.random() * 6);
+		/*调用 洞口 随机数*/
+		countWinNumFun();
+		
+		if(!gameContinue){
+			return  ;
+		}
+//		num = Math.floor(Math.random() * 6);
+		console.debug("num",num);
 
 		var childImg = '<img src="images/0715_images/act_16coolIceBucket_game_hotPeople.png" id="gamePlayChilren" alt="">';
 
 		$(".gameWin").eq(num).find(".gameWinMainWrap").append(childImg);
 
 		$("#gamePlayChilren").one("touchstart", function(e) {
-			
-			windowAniDownTimeBoo = false;
+			if(!gameContinue){
+				return  ;
+			}
+			$("#gamePlayChilren").remove();
 			/*调用  冰桶击中 分数统计方法*/
 			scoreCountFun(num);
-			musicConSrc=sound_add;
-			musicConSrc.currentTime = 0;
-			musicConSrc.play();
+//			musicConSrc=sound_add;
+			sound_add.currentTime = 0;
+			sound_add.play();
 			e.preventDefault();
 		});
 
@@ -54,54 +87,24 @@ $(function() {
 		$("#gamePlayChilren").animate({
 			"transform": "scale(" + scaleNum + ")",
 			"-webkit-transform": "scale(" + scaleNum + ")"
-		}, 300, function() {
-			clearTimeout(windowAniDownTime);
-			clearTimeout(clickTime);
-			windowAniDownTime = setTimeout(function() {
-				windowAniDown(num);
-				//                  windowAniDownFun(num);
-			}, 300)
-		});
-	};
-	var windowAniUp = windowAniUpFun;
-	/*人物消失*/
-	function windowAniDownFun(i) {
-		if (windowAniDownTimeBoo) {
-			console.log(1)
+		}, 500, function() {
+			
 			$("#gamePlayChilren").animate({
 				"transform": "scale(0)",
+				'opacity':0,
 				"-webkit-transform": "scale(0)"
-			}, 100, function() {
-				$("#gamePlayChilren").remove();
-				$("#scoreShowImg").remove();
+			}, 500, function() {
 				setTimeout(function() {
-					windowAniUp();
-					//	                windowAniUpFun();
-				}, 200);
-				scoreOnceBoo = true;
-			});
-		} else {
-			console.log(2)
-			setTimeout(function() {
-				$("#gamePlayChilren").animate({
-					"transform": "scale(0)",
-					"-webkit-transform": "scale(0)"
-				}, 100, function() {
 					$("#gamePlayChilren").remove();
-					$("#scoreShowImg").remove();
-					setTimeout(function() {
-						windowAniUp();
-						//  		                windowAniUpFun();
-					}, 200);
-					scoreOnceBoo = true;
-				});
-			}, 300)
-		}
-
+					$("#gamePlayChilren").unbind('touchstart') ;
+					if(gameContinue){
+						windowAniUpFun();
+					}
+				}, 200);
+			});
+		});
 	};
-	var windowAniDown = windowAniDownFun;
 	/*冰桶 浇中 ，分数计算函数
-	 
 	 * 这个方法处理   以下事件
 	 * 1、点击出现的人物
 	 * 2、点击人物  ，人物被击中
@@ -112,10 +115,10 @@ $(function() {
 	 * */
 	function scoreCountFun(wrapNum) {
 		var ScoreShow = '<img src="images/0715_images/act_16coolIceBucket_game_score.png" id="scoreShowImg" alt="">';
-		var bucketImg = '<img src="images/0715_images/act_16coolIceBucket_game_IceBucket.png" id="gamebucket" alt="">';
 		var nohotchild = '<img src="images/0715_images/act_16coolIceBucket_game_nohotPeople.png" id="nohotChilren" alt="">';
 		var bucketImg_water = '<img src="images/0715_images/act_16coolIceBucket_game_IceWater.png" id="gamebucket_water" alt="">';
-
+		var bucketImg = '<img src="images/0715_images/act_16coolIceBucket_game_IceBucket.png" id="gamebucket" alt="">';
+//
 		
 		$(".gameWin").eq(wrapNum).find(".gameWinMainWrap").append(bucketImg);
 		setTimeout(function() {
@@ -125,17 +128,12 @@ $(function() {
 			/*重新换一个桶*/
 			$(".gameWin").eq(wrapNum).find(".gameWinMainWrap").append(bucketImg_water);
 			$(".gameWin").eq(wrapNum).find(".gameWinMainWrap").append(ScoreShow);
-			
-			/*热的人物消失*/
-			$("#gamebucket").remove();
 			/*重新换一个人物*/
 			$(".gameWin").eq(wrapNum).find(".gameWinMainWrap").append(nohotchild);
 
 			setTimeout(function() {
 				$("#scoreShowImg").remove();
-				
 				$("#nohotChilren").remove();
-
 				$("#gamebucket_water").remove();
 			}, 300)
 		}, 500)
@@ -153,30 +151,16 @@ $(function() {
 		var countDownWrapW = window.getComputedStyle(widthWrap, null).width;
 		countDownTime = setInterval(function() {
 			timeCountDown--;
-			if (timeCountDown <= 0) {
+			if (timeCountDown < 1) {
+				gameContinue = false;
 				$("#gamePlayChilren").remove();
 				$("#scoreShowImg").remove();
-				clearTimeout(windowAniDownTime);
-				clearTimeout(clickTime);
-				windowAniUp = null;
 				windowAniDown = null;
 				clearInterval(countDownTime);
 				timeCountDown = 0;
-				//              if(gameScore<=15){
-				//                  var finalTextShow="颜值太低，惨不忍睹，还是多爱惜自己一些吧！";
-				//              }else if(gameScore>15 && gameScore <=30){
-				//                  var finalTextShow="颜值有待提高，多保养，相信自己一定会貌美如花的！";
-				//              }else if(gameScore>30 && gameScore<=45){
-				//                  var finalTextShow="恭喜你，收获"+gameScore+"颜值，沉鱼落雁，闭月羞花说的就是你吧！";
-				//              }else if(gameScore>45){
-				//                  var finalTextShow="天啊！世间竟有如此倾国倾城的女子！天仙姐姐，约吗？";
-				//              };
-				//              wechatObj.title = '【药快到】提高颜值大作战';
-				//              wechatObj.desc ='我刚刚在【药快到】美丽大作战中测了一下颜值，快来和我比比谁更美！';
-				//              getWechatObj();
+			
 				$("#gameScoreDesText").html("恭喜您成功帮助" + gameScore + "人降温， 这个夏天我要凉爽凉爽哒");
 
-				//              $("#gamebucket_water").remove();
 				$(".gameOverWrap").show();
 			};
 			var countDownShowW = Math.ceil(timeCountDown / 30 * 100);
@@ -187,34 +171,11 @@ $(function() {
 		}, 1000)
 	}
 
-	/*初始化函数*/
-	function initFun() {
-		windowAniUp = windowAniUpFun;
-		windowAniDown = windowAniDownFun;
-		//		 windowAniUpFun();
-		//       windowAniDownFun(0);
-		gameScore = 0; //分数统计
-		windowAniDownTimeBoo = true; //是否点击
-		num = 0; //指定当前窗口下标
-		childChoNum = 0; //指定当前出现分数
-		windowAniDownTime = null;
-		clickTime = null;
-		$(".gameWinPinkMask").animate({
-			"transform": "translateY(0)",
-			"-webkit-transform": "translateY(0)"
-		}, 100);
-		$("#gamePlayChilren").remove();
+	/*再次游戏*/
+	$("#playAgain").on("click", function() {
 		$("#scoreShowImg").remove();
 		$(".gameOverWrap").fadeOut();
-		$(".startGameBtnWrap").fadeIn();
-		$("#gameCountDownTime").html(30);
-		$("#gameScore").html(0);
-		$("#gameCountDownColor").css({
-			"width": "100%"
-		});
-	}
-	$("#playAgain").on("click", function() {
-		initFun();
+		gameStart() ;
 	});
 
 	//分享按钮
