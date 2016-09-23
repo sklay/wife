a2cf8e94f7cea8c83b7e150885992dd9ce0c6d1f
1988fuyu@163.com <1988fuyu@163.com>
2016-09-23 18:02:08 +0800

    推箱子 9.23 修改

--- catch-cat/js/act_16pushBox_index0905.js
+++ catch-cat/js/act_16pushBox_index0905.js
@@ -1,7 +1,8 @@
 $(function() {
 	var target = 0 ;
 	var audio_music = document.getElementById('audioBackMusic');
-
+	var blackStore = [1,2,11,12,24,29,31,34] ;
+	var succFlag = [1,2,9,22] ;
 	/**音乐图标*/
 	$('.gameMusic_wrap').on('tap', function() {
 
@@ -24,18 +25,15 @@ $(function() {
 
 	/* 游戏主函数 */
 	function gameStart() {
-		//	timeCountDownFun();
-			games();
-			//TODO 先注释音乐 调试的时候太吵了
+		this.isOver = 0;
+		games();
+		//TODO 先注释音乐 调试的时候太吵了
 //		if(playMusic) {
 //			$('.gameMusic_wrap').show().find('img').removeClass('musicPause').addClass('musicPlay');
 //			audio_music.play();
 //		}
 
 	};
-//	var people_x = 5;
-//	var people_y = 5;
-	//n表示 关数
 	function games() {
 
 		speller = {
@@ -64,10 +62,8 @@ $(function() {
 				//  九个格子的位置
 				this.positions = [];
 				this.positions2 = [];
-				//默认难易度    如果  n 没有传 则是5 （黑块 移动on个次数）
-				//this.hard = n || 5;
-
-				//this.step = 0;  记录 移动的步 数
+				// 全局变量 ，游戏是否开始， 0表示未开始
+				this.isOver = 0;
 				this.blank = 6;
 				this.useTime = 15;
 				this.tag = 'li';
@@ -75,10 +71,9 @@ $(function() {
 				this.content2 = 'ul.box2';
 				this.lastIndex = this.blank;
 				//初始化 布局
-//				this.creatBJ();
 				this.createGrid();
 				//倒计时
-			//	this.clearShu();
+				this.clearShu();
 			},
 			
 			clearShu:function(){
@@ -91,9 +86,8 @@ $(function() {
 						console.debug("clear")
 						clearInterval(speller.timer);
 
-						clickBoo = false;
 						$(".gameFailWrap").fadeIn(500);
-
+						speller.isOver = 1;
 						if(playMusic) {
 							$(".gameMusic_wrap").find('img').removeClass('musicPlay').addClass('musicPause');
 							audio_music.pause();
@@ -153,43 +147,100 @@ $(function() {
 				$(this.content + ' li').on('tap', function() {
 
 					//从0到63 的任何一个数 在2维数组中的 位置 表示pos/8  i的位置  ,pos%8  j的位置
-					var pos = $(this).attr('data-pos') * 1;
-					target = pos ;
+					var boxPos = $(this).attr('data-pos') * 1;
+					var rowLine = (boxPos % 6 == 0) ? Math.ceil(boxPos / 6) + 1 : Math.ceil(boxPos / 6);
+					var rowMin = (rowLine-1)*6 ;
+					var rowMax = rowLine*6-1 ;
+					target = boxPos ;
 					var $this = $(this);
 //					console.log(this.people_x+"，"+this.people_y);
-					//		alert(pos);
 					
-					var person_index = $('a[data-flag=3]').parent().attr('data-pos') *1;
+					var personPos = $('a[data-flag=3]').parent().attr('data-pos') *1;
 					
 					
-					console.debug("canGo -> " + canGo) ;
-					var differ = person_index - pos ;
+					var differ = personPos - boxPos ;
+					
+					/**点击的是四周邻居*/
 					if(Math.abs(differ) == 6 || Math.abs(differ) == 1){
-						
-						/**道路 或者 旗子*/
+						/**点击的是 道路 或者 旗子*/
 						if($this.find('a[data-flag=0]').length > 0){
 							console.debug("道路 或 旗帜") ;
-							speller.onClickByIndex(parseInt(pos / 6), pos % 6);
+							_speller.exChange(boxPos,personPos) ;
 						}
 						/**木箱子 就要判断该线路下一个 */
 						else if($this.find('a[data-flag=2]').length > 0){
-							console.debug("木箱") ;
-							var f = differ > 0 ? -1 : 1 ;
-							var nNextPos = f*Math.abs(differ)*2 + person_index ;
-							console.debug(" person_index is  "+ person_index + ' cur pos is  ' + pos + ' , nNextPos --> ' + nNextPos) ;
-							/**判断箱子的下一个是不是道路或旗子*/
-							if($('li[data-pos='+nNextPos+'] a[data-flag=0]').length > 0){
-								speller.onClickByIndex(parseInt(pos / 6), pos % 6);
+							switch (differ){
+								/**往上走**/
+								case 6:{
+									/**箱子要移动的新位置*/
+									var boxNextPos = boxPos  - 6 ;
+									
+									console.debug("往上走    boxNextPos ---->" + boxNextPos) ;
+									/**判断箱子的下一个位子是否能移动*/
+									if($('li[data-pos='+boxNextPos+'] a[data-flag=0]').length > 0){
+										console.debug("往上走    可以走 ") ;
+										/**箱子跟人都往上跑一格*/
+										_speller.exChange(boxPos,boxNextPos) ;
+										_speller.exChange(boxPos,personPos) ;
+									}
+								}
+									break;
+									/**往右走**/
+								case -1:{
+									var boxNextPos = boxPos  + 1 ;
+									/**溢出右边界*/
+									if(boxNextPos > rowMax){
+										return ;
+									}
+									/**判断箱子的下一个位子是否能移动*/
+									if($('li[data-pos='+boxNextPos+'] a[data-flag=0]').length > 0){
+										/**箱子跟人都往上跑一格*/
+										_speller.exChange(boxPos,boxNextPos) ;
+										_speller.exChange(boxPos,personPos) ;
+									}
+								}
+									break;
+									/**往下走**/
+								case -6:{
+									var boxNextPos = boxPos  + 6 ;
+									/**判断箱子的下一个位子是否能移动*/
+									if($('li[data-pos='+boxNextPos+'] a[data-flag=0]').length > 0){
+										/**箱子跟人都往上跑一格*/
+										_speller.exChange(boxPos,boxNextPos) ;
+										_speller.exChange(boxPos,personPos) ;
+									}
+								}
+									break;
+									/**往左走**/
+								case 1:{
+									var boxNextPos = boxPos  - 1 ;
+									if(boxNextPos < rowMin){
+										return ;
+									}
+									/**判断箱子的下一个位子是否能移动*/
+									if($('li[data-pos='+boxNextPos+'] a[data-flag=0]').length > 0){
+										/**箱子跟人都往上跑一格*/
+										_speller.exChange(boxPos,boxNextPos) ;
+										_speller.exChange(boxPos,personPos) ;
+									}
+								}
+									break;
+								default:
+									break;
 							}
 						}
 						
-					}else {
-						var canGo = _speller.canGo(person_index) ;
+						
+					}
+					/**跳走*/
+					else {
+						var canGo = _speller.canGo(personPos) ;
+						console.debug(" canGo is " + canGo) ;
 						if(canGo){
-							speller.onClickByIndex(parseInt(pos / 6), pos % 6);
+							_speller.exChange(boxPos,personPos) ;
 						} 
 					}
-					
+					_speller.isSucc(); //游戏成功
 				});
 
 			},
@@ -202,125 +253,186 @@ $(function() {
 
 				$nBlank.html($blankA);
 				$blank.html($nBlankA);
-			},
-			//点击游戏小图片时，传入相应位置。例，4，5
-			onClickByIndex: function(i, j) {
-//              console.log("onClickByIndex-->"+i+","+j+",this.tArray[i][j]-->"+this.tArray[i][j]);
-				//				alert(i+","+j);
-				if(this.tArray[i][j] == 1 || this.tArray[i][j] == 3) {
-					//点的是墙或自己人物，或黑色区域
-					console.log("点的是强 或自己人物，或黑色区域");
-					return; //什么也不操作
-				}
-				if(this.tArray[i][j] == 2) { //点击的是箱子
-					var index = this.getMoveBox(i, j); //0表示不能移动，1左，2上，3下，4右。
-					console.log("0表示不能移动，1左，2上，3下，4右。index-->"+index);
-//					alert("箱子     "+index);
-					if(index == 0) { //不能移动
-						return; //什么也不操作
-					}
-					/*（注意，顺序不能错，先交换物，再交换箱子）*/
-					if(index == 1) { //箱子向左。
-						/*箱子的位置向哪个方向移动*/
-						this.changeImgAndData(i, j - 1, i, j); //交换
-					} else if(index == 2) {
-						this.changeImgAndData(i - 1, j, i, j); //交换
-					} else if(index == 3) {
-						this.changeImgAndData(i + 1, j, i, j); //交换
-					} else if(index == 4) {
-						this.changeImgAndData(i, j + 1, i, j); //交换
-					}
-					/*人向哪个方向移动*/
-					this.changeImgAndData(i, j, this.people_x, this.people_y); //人的交换
-					/*设置人的当前位置*/
-					this.people_x = i;
-					this.people_y = j;
-					this.isGameOver(); //游戏是否结束
-				} else if(this.tArray[i][j] == 0) { //点击的是空白处
-						/*（注意，顺序不能错，先交换物，再交换箱子）*/
-						/*人物位置数据与当前点击的位置数据进行交换。*/
-//						console.log("人物位置数据与当前点击的位置数据进行交换。this.people-->"+(this.people_x)+","+this.people_y);
-						this.changeImgAndData(i, j, this.people_x, this.people_y);
-						/*设置人的当前位置*/
-						this.people_x = i;
-						this.people_y = j;
-//						console.log("设置人的当前位置。this.people-->"+this.people_x+","+this.people_y);
-//						this.changeImgAndData(i, j, this.people_x, this.people_y);
-//						/*设置人的当前位置*/
-//						this.people_x = i;
-//						this.people_y = j;
-				}
-			},
-
-			//箱子可移动的 方法   
-			//0表示不能移动，1左，2上，3下，4右。
-			getMoveBox: function(i, j) {
-				//9：黑图，0：占位透明图，1：墙，2：可移动的箱子，3：可移动的小人 
 				
-				//如果左边是透明的 图  右边是小人  表示箱子 可以往左移动  返回1
-				if(this.tArray[i][j - 1] == 0 && this.tArray[i][j + 1] == 3) {
-					// i  不变；j-1     根据这个 坐标 取到的数= 0 表示  左边是 透明的 墙  
-					return 1;
-				} 
-				//如果上面是 透明的 图   下面是小人 表示箱子可以往上移动  返回2
-				else if(this.tArray[i - 1][j] == 0 && this.tArray[i + 1][j] == 3) {
-					//向上移动    i-1  j不变
-					return 2;
-
-				} else if(this.tArray[i][j + 1] == 0 && this.tArray[i][j - 1] == 3) {
-					//向右移动    i不变  不变；j+1
-					return 4;
-
-				} else if(this.tArray[i + 1][j] == 0 && this.tArray[i - 1][j] == 3) {
-					//向下移动    i+1  不变；j
-					return 3;
-				} else {
-					return 0;
+				this.checkDown() ;
+			},
+			
+			checkDown :function(){
+				var temp = 0 ;
+				var boxs = $('li a[data-flag=2]') ;
+				var boxLength = boxs.length ;
+				/***/
+				$.each(boxs, function(i ,box){
+					var pos = $(box).parent().attr('data-pos')*1;
+					if($.inArray(pos ,blackStore) != -1){
+						temp ++ ;
+					}
+				})
+				//如果没有箱子移动 表示失败
+				if(speller.useTime > 1) {
+					if(temp == boxLength){
+						console.debug("无路可走") ;
+	//					if(speller.useTime > 1) {
+							console.debug("clear")
+							clearInterval(speller.timer);
+	
+							$(".gameFailWrap").fadeIn(500);
+							speller.isOver = 1;
+							if(playMusic) {
+								$(".gameMusic_wrap").find('img').removeClass('musicPlay').addClass('musicPause');
+								audio_music.pause();
+							}
+	//					}
+					}
 				}
-
 			},
-			//根据下标交换图片与数据
-			changeImgAndData: function(i1, j1, i2, j2) {
-				/*1、交换图片，其中数据为0的图片是秀明的*/
-				//图片的交换
-				var imgId1 = i1 * 6 + j1;
-				var imgId2 = i2 * 6 + j2;
-				var imgori;
+			
+//			//点击游戏小图片时，传入相应位置。例，4，5
+//			onClickByIndex: function(i, j) {
+//				if(this.tArray[i][j] == 1 || this.tArray[i][j] == 3) {
+//					//点的是墙或自己人物，或黑色区域
+//					console.log("点的是强 或自己人物，或黑色区域");
+//					return; //什么也不操作
+//				}
+//				if(this.tArray[i][j] == 2) { //点击的是箱子
+//					var index = this.getMoveBox(i, j); //0表示不能移动，1左，2上，3下，4右。
+//					console.log("0表示不能移动，1左，2上，3下，4右。index-->"+index);
+////					alert("箱子     "+index);
+//					if(index == 0) { //不能移动
+//						return; //什么也不操作
+//					}
+//					/*（注意，顺序不能错，先交换物，再交换箱子）*/
+//					if(index == 1) { //箱子向左。
+//						/*箱子的位置向哪个方向移动*/
+//						this.changeImgAndData(i, j - 1, i, j); //交换
+//					} else if(index == 2) {
+//						this.changeImgAndData(i - 1, j, i, j); //交换
+//					} else if(index == 3) {
+//						this.changeImgAndData(i + 1, j, i, j); //交换
+//					} else if(index == 4) {
+//						this.changeImgAndData(i, j + 1, i, j); //交换
+//					}
+//					/*人向哪个方向移动*/
+//					this.changeImgAndData(i, j, this.people_x, this.people_y); //人的交换
+//					/*设置人的当前位置*/
+//					this.people_x = i;
+//					this.people_y = j;
+//					
+//					
+//					this.isSucc(); //游戏成功
+//				} else if(this.tArray[i][j] == 0) { //点击的是空白处
+//					//判断点击 空位置的时  人物 是否可以和空位子交换
+//					//剩下的任意位置都能交换  所以不用判断和哪个方位交换
+//					/*（注意，顺序不能错，先交换物，再交换箱子）*/
+//					/*人物位置数据与当前点击的位置数据进行交换。*/
+//					//console.log("人物位置数据与当前点击的位置数据进行交换。this.people-->"+(this.people_x)+","+this.people_y);
+//					this.changeImgAndData(i, j, this.people_x, this.people_y);
+//					/*设置人的当前位置*/
+//					this.people_x = i;
+//					this.people_y = j;
+//					console.log("设置人的当前位置。this.people-->"+this.people_x+","+this.people_y);
+//					
+//				}
+//			},
 
-				//图片的位置
-//				console.log("==changeImgAndData,imgId1-->"+imgId1+",this.tag)-->"+this.tag);
-				var $blank = $(this.content).find(this.tag).eq(imgId1);
-//				console.log("==changeImgAndData,$blank-->"+$blank);
-				//图片2位置
-//				console.log("==changeImgAndData,imgId1-->"+imgId2+",this.tag)-->"+this.tag);
-				var $nBlank = $(this.content).find(this.tag).eq(imgId2);
-//				console.log("==changeImgAndData,$blank-->"+$nBlank);
-				//  获取 黑块中的 内容  clone(false)  此方法表示只复制 内容  不复制 方法
-				var $blankA = $blank.children().clone(false);
-				var $nBlankA = $nBlank.children().clone(false);
-
-				$nBlank.html($blankA);
-				$blank.html($nBlankA);
-
-				/*2、交换数据*/
-				//alert(i2+","+j2);
-				var t = this.tArray[i1][j1];
-				this.tArray[i1][j1] = this.tArray[i2][j2];
-				this.tArray[i2][j2] = t;
+//			//箱子可移动的 方法   
+//			//0表示不能移动，1左，2上，3下，4右。
+//			getMoveBox: function(i, j) {
+//				//9：黑图，0：占位透明图，1：墙，2：可移动的箱子，3：可移动的小人 
+//				
+//				//如果左边是透明的 图  右边是小人  表示箱子 可以往左移动  返回1
+//				if(this.tArray[i][j - 1] == 0 && this.tArray[i][j + 1] == 3) {
+//					// i  不变；j-1     根据这个 坐标 取到的数= 0 表示  左边是 透明的 墙  
+//					return 1;
+//				} 
+//				//如果上面是 透明的 图   下面是小人 表示箱子可以往上移动  返回2
+//				else if(this.tArray[i - 1][j] == 0 && this.tArray[i + 1][j] == 3) {
+//					//向上移动    i-1  j不变
+//					return 2;
+//
+//				} else if(this.tArray[i][j + 1] == 0 && this.tArray[i][j - 1] == 3) {
+//					//向右移动    i不变  不变；j+1
+//					return 4;
+//
+//				} else if(this.tArray[i + 1][j] == 0 && this.tArray[i - 1][j] == 3) {
+//					//向下移动    i+1  不变；j
+//					return 3;
+//				} else {
+//					return 0;
+//				}
+//
+//			},
+//			//根据下标交换图片与数据
+//			changeImgAndData: function(i1, j1, i2, j2) {
+//				/*1、交换图片，其中数据为0的图片是秀明的*/
+//				//图片的交换
+//				var imgId1 = i1 * 6 + j1;
+//				var imgId2 = i2 * 6 + j2;
+//				var imgori;
+//
+//				//图片的位置
+////				console.log("==changeImgAndData,imgId1-->"+imgId1+",this.tag)-->"+this.tag);
+//				var $blank = $(this.content).find(this.tag).eq(imgId1);
+////				console.log("==changeImgAndData,$blank-->"+$blank);
+//				//图片2位置
+////				console.log("==changeImgAndData,imgId1-->"+imgId2+",this.tag)-->"+this.tag);
+//				var $nBlank = $(this.content).find(this.tag).eq(imgId2);
+////				console.log("==changeImgAndData,$blank-->"+$nBlank);
+//				//  获取 黑块中的 内容  clone(false)  此方法表示只复制 内容  不复制 方法
+//				var $blankA = $blank.children().clone(false);
+//				var $nBlankA = $nBlank.children().clone(false);
+//
+//				$nBlank.html($blankA);
+//				$blank.html($nBlankA);
+//
+//				/*2、交换数据*/
+//				//alert(i2+","+j2);
+//				var t = this.tArray[i1][j1];
+//				this.tArray[i1][j1] = this.tArray[i2][j2];
+//				this.tArray[i2][j2] = t;
+//			},
+			
+			isSucc: function() {
+				console.debug('媳妇说没走')
+				var temp = 0 ;
+				var boxs = $('li a[data-flag=2]') ;
+				/***/
+				$.each(boxs, function(i ,box){
+					var pos = $(box).parent().attr('data-pos')*1;
+					if($.inArray(pos ,succFlag) != -1){
+						temp ++ ;
+					}
+				})
+				/**长度相等时说明 箱子已经把旗帜盖住了**/
+				if(temp != succFlag.length){
+					return  ;
+				}
+				
+				$(".gameSuccWrap").fadeIn(500);
+				speller.isOver = 1;
+				if(playMusic) {
+					$(".gameMusic_wrap").find('img').removeClass('musicPlay').addClass('musicPause');
+					audio_music.pause();
+				}
 			},
-
-			isGameOver: function() {
+			
+			isSucc_: function() {
+				console.debug('媳妇说没走')
 				//this.mArray[i][0] 取的是 二维数据mArray 的横坐标的值
 				//this.mArray[i][1]] 取的是 二维数据mArray 的纵坐标的值
 				//根据 上面坐标的值   二维数据找出 tArray 对应的位置上的值   如果不是2  说明 箱子还没有移到国旗的位置，否则游戏胜利
 				for(var i = 0; i < this.mArray.length; i++) {
 					if(this.tArray[this.mArray[i][0]][this.mArray[i][1]] != 2) {
-						//判断箱子是不是可以移动   可以给一个限制的时间   比如 30秒内 没有箱子移动了 
+						console.debug("此时数组重的值");
 						return;
 					}
 				}
-//				alert("成功了");
 				$(".gameSuccWrap").fadeIn(500);
+				speller.isOver = 1;
+				if(playMusic) {
+					$(".gameMusic_wrap").find('img').removeClass('musicPlay').addClass('musicPause');
+					audio_music.pause();
+				}
 			},
 			canGo : function(cat) {
 				var rst = [] ;
@@ -455,23 +567,9 @@ $(function() {
 		speller.init();
 	}
 
-	//点击查看显示原图
-	$(".showOriginBtn").on("click", function(e) {
-		var event = e || window.event;
-		event.stopPropagation();
-		$(".game_origin").show();
-	});
-	$(".game_origin").on("tap", function(e) {
-		var event = e || window.event;
-		event.stopPropagation();
-		$(".game_origin").hide();
-	});
-
 	/* 再次游戏 */
-	clickBoo = false;
 	$("#playAgain").on("click", function(e) {
-		if(!clickBoo) {
-			clickBoo = true;
+		if (speller.isOver == 1) {
 			$(".gameFailWrap").fadeOut();
 			gameStart();
 		}
