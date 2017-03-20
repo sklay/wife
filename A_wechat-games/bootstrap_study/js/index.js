441e868bfac694affb1e3843d08d3e17dc2e5716
1988fuyu@163.com <1988fuyu@163.com>
2017-03-20 17:27:09 +0800

    2017-3-20 layui学习 弹框

---
+++ A_wechat-games/bootstrap_study/js/index.js
@@ -0,0 +1,46 @@
+$(function() {
+
+	var lis = $(".skip").find("ul li");
+
+	$(lis).click(function() {
+
+		var skipElement = $(this).find("a").attr("data-value");
+
+		var skipLoaction = $("." + skipElement).position().top + $("." + skipElement).offsetParent().offset().top;
+
+		getLocation(skipLoaction - 50)
+	});
+
+
+	var hs = $(".container>.content>.left>ul>li>h2");
+	$(hs).click(function() {
+
+		var currentUl = $(this).siblings("ul");
+		var currentlis = currentUl.find("li");
+		if(currentUl.attr("class").indexOf("in")>-1) {
+
+			for(var i = 0; i < currentlis.length; i++) {
+
+				if($(currentlis).eq(i).attr("class") == "currentLink") {
+
+					$(this).attr("class", "currentCategory");
+				};
+
+			};
+
+		}else{
+			
+			$(this).removeClass("currentCategory");
+		}
+
+	});
+
+});
+
+function getLocation(skipLoaction) {
+
+	$("body,html").animate({
+		scrollTop: skipLoaction
+	}, 500);
+
+}
