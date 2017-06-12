e49d8524157c5b5e3c31ed9fa7bf1a87507c0a01
1988fuyu@163.com <1988fuyu@163.com>
2017-06-12 17:22:05 +0800

    2017.6.12 vue 完成计划表

--- A_wechat-games/vue_study/js/app.js
+++ A_wechat-games/vue_study/js/app.js
@@ -1,32 +1,94 @@
-var list = [
+//2017.6.12 新加功能
+
+//存储localStorage里的数据
+var store = {
+	save (key,value){
+		localStorage.setItem(key,JSON.stringify(value));
+	},
+	fetch(key){
+		return JSON.parse(localStorage.getItem(key)) || [];
+	}
+}
+
+var list = store.fetch("zhou-class")
+
+/*分割线   ---之前写的*/
+/*var list = [
 
 	{
 		title:"吃饭睡觉打豆豆",
-		isChecked:false
+		isChecked:false,
 	},
 	{
 		title:"吃饭睡觉打豆豆",
-		isChecked:true
+		isChecked:true,
 	}
-];
-new Vue({
+];*/
+//过滤有三种情况  all  finished  unfinished
+			var filter = {
+				all:function(list){
+					return list;
+				},
+				finished:function(list){
+					return list.filter(function(item){
+						return item.isChecked;
+					})
+				},
+				unfinished:function(list){
+					return list.filter(function(item){
+						return !item.isChecked;
+					})
+				}
+			}
+
+var vm = new Vue({
 	el:".main",
 	data:{
 		list:list,
 		todo:"",
 		edtorTodos:"",//记录 正在 编辑的数据
-		beforeTitle:""//记录正在编辑的这天任务的 title
+		beforeTitle:"",//记录正在编辑的这天任务的 title
+		
+		
+		//2017.6.12新添
+		visibility:"all"//通过这个数值的变化  对数据进行筛选
 	},
+	
+//2017.6.12 新加功能
+
+watch:{
+//	list:function(){//监控list这个属性，但这个属性对应的值发生变化就会执行函数
+//		store.save("zhou-class",this.list);
+//		
+//	}
+
+//深层次的监控
+list:{
+	handler:function(){
+		store.save("zhou-class",this.list);
+	},
+	deep:true
+}
+
+
+},
+	
 	computed:{
 		noCheckeLength:function(){
 			return this.list.filter(function(item){
 				return  !item.isChecked
 			}).length
+		},
+		//2017.6.12新添
+		//过滤数据
+		filteredList:function(){
+			//找到过滤函数，就返回过滤后的数据，如果没有就返回所有的数据
+			return filter[this.visibility] ? filter[this.visibility](list) : list;
 		}
 	},
 	methods:{
 		//addTodo(data,ev){ 
-		addTodo(){ //添加任务
+		addTodo(){ //添加任务 (简写形式，es6语法)  ---》    addTodo:function(){}
 			this.list.push({
 				title:this.todo,
 				isChecked:false
@@ -44,7 +106,7 @@ new Vue({
 			
 			this.edtorTodos = todo;
 		},
-		edtorTodoed(todo){//编辑任务
+		edtorTodoed(todo){//编辑完成
 			this.edtorTodos = "";
 		},
 		cancleTodo(todo){//取消编辑
@@ -59,7 +121,7 @@ new Vue({
 		"focus":{
 			update(el,binding){//钩子函数      el是指令绑定的 元素   update是一个 钩子函数
 				console.log("el是 ======",el);
-				console.log("binding=====",binding);  //里面有个值 value  看这个 值  是为 true  还是 foucs
+				console.log("binding=====",binding);  //里面有个值 value  看这个 值  是为 true  还是 false
 				
 				
 				if(binding.value){
@@ -69,3 +131,15 @@ new Vue({
 		}
 	}
 });
+
+function washHashChange(){
+	var hash = window.location.hash.slice(1);
+	console.log(hash);
+	
+	
+	vm.visibility = hash;
+}
+
+washHashChange();
+
+window.addEventListener("hashchange",washHashChange);
